68b7aac49698e5effe56b5d06a9a4737f1aa5007
Jake Renzella <j@kerenzella.com>
2016-07-21 10:53:45 +1000

    FIX: Add missing permissions to helpdesk ticket

--- app/models/helpdesk_ticket.rb
+++ app/models/helpdesk_ticket.rb
@@ -9,6 +9,44 @@ class HelpdeskTicket < ActiveRecord::Base
   # Model constratins/validation
   validates :project, presence: true # Must always be associated to a project
 
+  #
+  # Permissions around group data
+  #
+  def self.permissions
+    # What can students do with all tickets?
+    student_role_permissions = [
+    ]
+    # What can tutors do with all tickets?
+    tutor_role_permissions = [
+      :get_tickets
+    ]
+    # What can convenors do with all tickets?
+    convenor_role_permissions = [
+      :get_tickets
+    ]
+    # What can admins do with all tickets?
+    admin_role_permissions = [
+      :get_tickets
+    ]
+    # What can nil users do with all tickets?
+    nil_role_permissions = [
+
+    ]
+
+    # Return permissions hash
+    {
+      :admin    => admin_role_permissions,
+      :convenor => convenor_role_permissions,
+      :tutor    => tutor_role_permissions,
+      :student  => student_role_permissions,
+      :nil      => nil_role_permissions
+    }
+  end
+
+  def self.role_for(user)
+    user.role
+  end
+
   # Returns back all unresolved tickets
   def self.all_unresolved
     where(is_resolved: false)
@@ -35,7 +73,7 @@ class HelpdeskTicket < ActiveRecord::Base
     project.student
   end
 
-  # Returns true iff ticket is associated with a task
+  # Returns true if ticket is associated with a task
   def has_task?
     !task.nil?
   end
