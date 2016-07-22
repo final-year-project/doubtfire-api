2f25caec1ee7bcffab4d689ec95bab4248a54979
Jake Renzella <j@kerenzella.com>
2016-07-22 14:42:01 +1000

    QUALITY: Make endpoint tests use auth helpers

--- test/api/helpdesk/ticket_test.rb
+++ test/api/helpdesk/ticket_test.rb
@@ -9,40 +9,36 @@ class TicketsTest < ActiveSupport::TestCase
     Rails.application
   end
 
-  def setup
-    @auth_token = get_auth_token()
-  end
-
   # --------------------------------------------------------------------------- #
   # --- Endpoint testing for:
   # ------- /helpdesk/tickets.json
   # ------- GET
-
   # --------------------------------------------------------------------------- #
   # POST tests
 
   # GET tests
   # Test GET for all tickets
-  def test_get_helpdesk_tickets
+  def test_get_heldpesk_tickets
     get with_auth_token "/api/helpdesk/ticket.json"
     actual_ticket = JSON.parse(last_response.body)[0]
   end
 
   # Test GET for a ticket with and id
-  def test_get_helpdesk_ticket_with_id
+  def test_get_heldpesk_ticket_with_id
     id = 0;
-    get "/api/helpdesk/ticket/#{id}.json?auth_token=#{@auth_token}"
+    get with_auth_token "/api/helpdesk/ticket/#{id}.json"
     actual_ticket = JSON.parse(last_response.body)[0]
+    puts "actual_tickets: #{actual_ticket}"
   end
 
   # Test POST for a ticket with and id
-  def test_post_helpdesk_ticket
-    @auth_token = auth_token_for(Project.first.user)
+  def test_post_heldpesk_ticket
     project_id = Project.first.id
     description = "jake renzella's comment :)"
 
-    post "/api/helpdesk/ticket.json?project_id=#{project_id}?description=#{description}?auth_token=#{@auth_token}"
+    post with_auth_token "/api/helpdesk/ticket.json?project_id=#{project_id}&description=#{description}", Project.first.user
 
     actual_ticket = JSON.parse(last_response.body)[0]
+    puts "actual_tickets: #{actual_ticket}"
   end
 end
