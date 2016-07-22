841a934a722efedfa0e0574d54f0dc41213c3350
Alex Cummaudo <alexcu@me.com>
2016-07-22 14:48:12 +1000

    QUALITY: Remove debug messages

--- test/api/helpdesk/ticket_test.rb
+++ test/api/helpdesk/ticket_test.rb
@@ -18,27 +18,25 @@ class TicketsTest < ActiveSupport::TestCase
 
   # GET tests
   # Test GET for all tickets
-  def test_get_heldpesk_tickets
+  def test_get_helpdesk_tickets
     get with_auth_token "/api/helpdesk/ticket.json"
     actual_ticket = JSON.parse(last_response.body)[0]
   end
 
   # Test GET for a ticket with and id
-  def test_get_heldpesk_ticket_with_id
+  def test_get_helpdesk_ticket_with_id
     id = 0;
     get with_auth_token "/api/helpdesk/ticket/#{id}.json"
     actual_ticket = JSON.parse(last_response.body)[0]
-    puts "actual_tickets: #{actual_ticket}"
   end
 
   # Test POST for a ticket with and id
-  def test_post_heldpesk_ticket
+  def test_post_helpdesk_ticket
     project_id = Project.first.id
     description = "jake renzella's comment :)"
 
     post with_auth_token "/api/helpdesk/ticket.json?project_id=#{project_id}&description=#{description}", Project.first.user
 
     actual_ticket = JSON.parse(last_response.body)[0]
-    puts "actual_tickets: #{actual_ticket}"
   end
 end
