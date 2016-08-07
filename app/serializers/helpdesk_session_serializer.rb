54f3ab8f64d7a1f51aaec43c17589dd1f0ae4502
Alex Cummaudo <alexcu@me.com>
2016-08-07 14:35:13 +1000

    ENHANCE: Add improved session serializer

---
+++ app/serializers/helpdesk_session_serializer.rb
@@ -0,0 +1,15 @@
+require 'user_serializer'
+
+class HelpdeskSessionSerializer < ActiveModel::Serializer
+  attributes :id,
+             :user,
+             :clocked_on?,
+             :clock_on_time,
+             :clock_off_time
+
+  has_one :user, serializer: ShallowUserSerializer
+
+  def clocked_on?
+    object.clocked_on?
+  end
+end
